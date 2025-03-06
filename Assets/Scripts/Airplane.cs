280d24a1b176dada3fc435d66b60d7028b29c828
Sundayx4 <thirat.chan@bumail.net>
2025-03-06 16:07:51 +0700

    เครื่องบบินไปข้างหน้า + ยกตัว

---
+++ Assets/Scripts/Airplane.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Airplane : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float enginePower = 20f;
+    [SerializeField] float liftBooster = 0.5f;
+    [SerializeField] float drag = 0.001f;
+    [SerializeField] float angularDrag = 0.001f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(transform.forward * enginePower);
+        }
+
+        Vector3 lift = Vector3.Project(rb.linearVelocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+    }
+}
